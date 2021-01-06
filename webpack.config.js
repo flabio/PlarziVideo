ce445439cdfa3c3beab4b3a2f00e0285ace2f012
flabio <flabiohs@gmail.com>
2021-01-06 07:34:48 -0500

    configuracion .eslintrc and .gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
@@ -25,6 +25,14 @@ module.exports = {
           loader: 'html-loader',
         },
       },
+      {
+        test: /\.(s*)css$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          'css-loader',
+          'sass-loader',
+        ],
+      }, 
     ],
   },
   plugins: [
@@ -32,5 +40,14 @@ module.exports = {
       template: './public/index.html',
       filename: './index.html',
     }),
+    new MiniCssExtractPlugin({
+        filename: 'assets/[name].css',
+      }),
   ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3005,
+  }
+
 };
